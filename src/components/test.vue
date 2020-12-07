1d69f7728daf735556ddd1c78e4caf9a39f04857
unknown <LUShuQiao@igandan.com>
2020-12-07 11:41:57 +0800

    11.41提交

---
+++ src/components/test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button ref="buttonA" @click="handClickA">buttonA</button>
+    <button @click="handClickB">buttonB</button>
+  </div>
+</template>
+<script>
+export default {
+  name:"login",
+  data() {
+    return {
+    };
+  },
+  methods: {
+    handClickA () {
+      console.log('buttonA上的点击事件触发了')
+    },
+    handClickB () {
+      this.$refs.buttonA.$el.click()
+    }
+  }
+};
+</script>
